f5b5afa3ed45a199f7eaef5a78de1603287573fd
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-14 18:01:34 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+  // This component has an initial state property called timesClicked, which is  initially defined as 0.
+
+  // The component renders out a button with a label that shows the timesClicked   value. This means that, at the start, your button should just say 0.
+
+  // Whenever the button is clicked, update the state by incrementing the  timesClicked by 1.
+  state = {
+    timesClicked: 0
+  }
+
+  handleClick = () => (
+    this.setState(previousState => {
+    return {
+      timesClicked: previousState.timesClicked + 1
+    }
+  }, console.log(this.state.timesClicked)))
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+  // Create a button with the class 'bitrate'. Clicking this button changes the settings.bitrate state property to 12.
+
+  // Create a button with the class 'resolution'. Clicking this button changes the settings.video.resolution state property to '720p'.
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+  
+  // changeBitrate = () => {
+  //   this.setState({
+  //     settings: {...this.state.settings, bitrate: 12}
+  //   })
+  // }
+
+  // pass setState method a callback 
+  changeBitrate = () => {
+    this.setState(previousState => {
+      return {
+        settings: {...previousState.settings, bitrate: 12}
+      }
+    })
+  }
+
+  changeResolution = () => {
+    this.setState(previousState => {
+      return {
+        settings: {...previousState.settings, video: { resolution: '720p'}}
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.changeBitrate}>Change Bitrate</button>
+        <button className="resolution" onClick={this.changeResolution}>Change Resolution</button>
+      </div>
+    )
+  }
+}
